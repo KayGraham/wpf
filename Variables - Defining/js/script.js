6a1cbe0faf6518ad7bbf58c56442bb0191e02724
KayGraham <KayGraham3@fullsail.edu>
2013-10-08 22:53:18 -0400

    Values and Variables

---
+++ Variables - Defining/js/script.js
@@ -0,0 +1,50 @@
+//JavaScript
+
+// var yearBorn = 1955;
+
+/*
+console.log("Hello World!");
+console.log(yearBorn);
+*/
+
+/*
+alert("Hello World!");
+*/
+
+//Defining Variables
+
+// var age; //declaration
+
+// age = 26; //definition
+
+// var yearOfBirth = 1970; //declaring and defining on the same line
+
+//storage container = value
+
+//Variables of Different Types, Numbers, Booleans and Strings
+
+//var age = 38; // Number variable
+
+//var isStudent = false; //Boolean Variable
+
+//var firstName = "Kayla";
+
+//var phase = 'I don\'t know. \nYou\'ll have to speak with the professor.';
+
+//console.log(phase);
+
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
+
+console.log(muppetNames[1]); //Shows what is in the index #1
+
+muppetNames[1] = "Rizzo"; //Changes index #1 to Rizzo
+muppetNames[2] = "Fozzy"; //Changes index #2 to Fozzy
+
+console.log(muppetNames[1]); //Shows what is in the index #1 after it is changed
+console.log(muppetNames[2]); //Shows what is in the index #2 after it is changed
+
+var num = 1;
+
+console.log(muppetNames[num]); //Shows what is in the index #1
