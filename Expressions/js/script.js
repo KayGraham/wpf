b4e17090285eb731b630711bee0369692285fd7d
KayGraham <KayGraham3@fullsail.edu>
2013-10-10 15:55:48 -0400

    Expressions

---
+++ Expressions/js/script.js
@@ -0,0 +1,143 @@
+//Expressions
+
+//var a = 2; //Declares variable a and defines it as 2
+
+//a = a + 3; //Adds 3 to a and stores the new value
+
+//console.log(a);
+
+//var a = 2; //Declares variable a and defines it as 2
+
+//var b;
+
+//b = a + 3; //Adds 3 to a and stores the new value as b
+
+//console.log(b);
+
+
+//Expressions - Age Example
+
+//var yearBorn = 1974;
+
+//var age = 2013 - yearBorn - 1;
+
+//console.log(age);
+
+
+//Expression - Arithmetic Operations
+
+//Area of a triagns is 1/2 W * H
+
+//var width = 8;
+
+//var height = 7;
+
+//var area;
+
+//area = width * height / 2;
+
+//console.log(area);
+
+
+//Expressions - Modulo Operator
+
+//var remainder = 9 % 2;
+
+//console.log(remainder);
+
+
+//Expressions - PEMDAS - Order of Operations
+
+//var quiz1 = 87;
+
+//var quiz2 = 100;
+
+//var quiz3 = 60;
+
+//var quiz4 = 80;
+
+//the average is the sum of the items devided by the number of items
+
+//var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
+
+//console.log(average);
+
+//var length = 7;
+
+//var width = 6;
+
+//var perimeter = length * 2 + width * 2;
+
+//console.log(perimeter);
+
+
+//Expressions - Expressions with Arrays
+
+//var orangeBins = [234, 567, 883];
+
+//var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+//console.log(total);
+
+
+//Expressions - Concatenating Strings
+
+//var firstName = "Kayla";
+
+//var lastName = "Robinson";
+
+//var fullName = firstName + " " + lastName;
+
+//console.log(fullName);
+
+//var a = "6";
+
+//var b = "7";
+
+//var a = 6;
+
+//var b = 7;
+
+//var result = a + b;
+
+//console.log(result);
+
+
+//Expression - Casting
+
+//var stringVar = "6";
+
+//var result = 7 + Number(stringVar);
+
+//console.log(result);
+
+//var areaCode = 407;
+
+//var firstPart = 203;
+
+//var secondPart = 4529;
+
+//(407)203-4529
+
+//var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" + String(secondPart);
+
+//console.log(phoneNo);
+
+
+//Expression - Assignment Operators
+
+//var a = 3;
+
+//a -= 4; //a = a - 4
+
+//console.log(a);
+
+var a = 3;
+
+//a ++ ; //a = a += 1
+
+a -- ; //a = a -= 1
+
+console.log(a);
+
+
